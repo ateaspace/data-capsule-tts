523c515ffabccaf6cb83a77547ea510c5f091023
David Bainbridge <davidb@waikato.ac.nz>
2020-12-16 05:35:58 -0500

    Ability to compile-up openssh if need (i.e., in the event the DEVEL script isn't run, because cmake is already on the system

--- mysql-complete/CASCADE-MAKE-PACKAGES-ALL.sh
+++ mysql-complete/CASCADE-MAKE-PACKAGES-ALL.sh
@@ -1,4 +1,13 @@
 
+if [ ! -d openssl-1.1.1h ] ; then
+    ./CASCADE-MAKE-OPENSSH.sh
+    status = $?
+    if [ $status != 0 ] ; then
+	echo "Failed to build openssh" >&2
+	exit $status
+    fi
+fi
+
 ./CASCADE-MAKE-NCURSES.sh \
     && ./CASCADE-MAKE-M4.sh \
     && ./CASCADE-MAKE-BISON.sh
