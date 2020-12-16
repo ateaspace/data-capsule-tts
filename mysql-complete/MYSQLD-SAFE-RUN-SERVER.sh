74da6456940e9e675a8c4aa59da357e6a95fddcd
David Bainbridge <davidb@waikato.ac.nz>
2020-12-16 05:24:20 -0500

    Returning this script to run mysqld_safe as the name of the script is indicative of.  Change still to be tested, however this change is in line with the changes with the MYSQLD-INIT.sh script, which now uses --initialize-insecure

--- mysql-complete/MYSQLD-SAFE-RUN-SERVER.sh
+++ mysql-complete/MYSQLD-SAFE-RUN-SERVER.sh
@@ -8,6 +8,10 @@ fi
 source ./_MYSQL-SETTINGS.bash
 
 
-"$MYSQL_COMPLETE_INSTALLED/mysql/bin/mysqld" \
-    --skip-grant-tables --user=$MYSQL_USER --port=$MYSQL_PORT \
+#"$MYSQL_COMPLETE_INSTALLED/mysql/bin/mysqld" \
+#    --skip-grant-tables --user=$MYSQL_USER --port=$MYSQL_PORT \
+#    --datadir=$MYSQL_DATA
+
+"$MYSQL_COMPLETE_INSTALLED/mysql/bin/mysqld_safe" \
+    --user=$MYSQL_USER --port=$MYSQL_PORT \
     --datadir=$MYSQL_DATA
