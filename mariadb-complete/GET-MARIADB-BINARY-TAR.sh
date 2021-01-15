89fd67ecdc55bb1aee22219b3d6c81b5178adacf
davidb <davidb@tanekaha.cms.waikato.ac.nz>
2021-01-15 14:31:34 +1300

    Useful binary to grab and use if working on linux 64-bit and looking to avoid compiling up mariadb code base

---
+++ mariadb-complete/GET-MARIADB-BINARY-TAR.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O mariadb-10.5.8-linux-x86_64.tar.gz "https://downloads.mariadb.org/interstitial/mariadb-10.5.8/bintar-linux-x86_64/mariadb-10.5.8-linux-x86_64.tar.gz/from/https%3A//mariadb.mirror.digitalpacific.com.au/"
